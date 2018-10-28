0e420d6a408598285580495a5f6a8f36f48712cf
Hymmnos <joshua_herrera@me.com>
2018-10-28 02:26:59 -0400

    Regressions.R heavily commented - easy to understand. Residuals by gridcells for full and reduced models exported to data folder for vis.

--- R_scripts/Regressions.R
+++ R_scripts/Regressions.R
@@ -2,7 +2,7 @@ library(caret)
 library(dplyr)
 library(leaps)
 library(MASS)
-
+library(car)
 
 home_dir <- ("G:/JoshuaData/Classes/MSDS61X0 Capstone/CapstoneProject")
 data_dir <- ("./data/")
@@ -11,7 +11,8 @@ plot_dir <- ("./plots/")
 
 setwd(home_dir)
 setwd(data_dir)
-# 
+
+# Binary predictor code - unused
 # infile <- "df_model_w_cell_id.csv"
 # df_model_w_cell_id <- read.csv(infile,
 #                                stringsAsFactors = FALSE, header = TRUE)
@@ -39,10 +40,8 @@ infile <- "df_model_w_cell_id.csv"
 df_model_w_cell_id <- read.csv(infile,
                                stringsAsFactors = FALSE, header = TRUE)
 
-
-
 # Drop unused data
-drop <- c("cell_id", "med_sale_pbo_n", "med_sale_pbo_y")
+drop <- c("med_sale_pbo_n", "med_sale_pbo_y")
 RegData <- df_model_w_cell_id[, !names(df_model_w_cell_id) %in% drop]
 
 # Drop >5-Cost incidents and 0-cost incidents
@@ -52,6 +51,8 @@ RegData <- RegData[!RegData$sum_cost_pedestrian_events > 5,]
 
 set.seed(0737)
 
+
+# Identify columns to log transform - unused
 
 # Col2Transform <- c("sum_cost_pedestrian_events",
 #                    "num_near_misses",            "n_rqst",                    "access",                    
@@ -74,76 +75,86 @@ set.seed(0737)
 # RegData[Col2Transform] <- log(RegData[Col2Transform])
 
 
-# # Output large results to txt file
-# sink("LRModel.fullint.txt")
-# print(summary(lm(sum_cost_pedestrian_events ~ (.)^2, RegData)))
+#####
+##### Begin Regressions
+#####
+
+# Basic regressions commented out to not waste procesing time. Already examined.
+# Simplest linear regression.
+# LRModel.full <- lm(sum_cost_pedestrian_events ~ . -cell_id, RegData)
+# sink("LRModel.full.txt")
+# print(summary(LRModel.full))
 # sink()
-# 
+
+# Linear regression with every combination of interaction
 # LRModel.fullint <- lm(sum_cost_pedestrian_events ~ (.)^2, RegData)
-# 
- sink("LRModel.full.txt")
- print(summary(lm(sum_cost_pedestrian_events ~ ., RegData)))
- sink()
- LRModel.full <- lm(sum_cost_pedestrian_events ~ ., RegData)
+# sink("LRModel.fullint.txt")
+# print(summary(LRModel.fullint))
+# sink()
 
-# Stepwise regressions.
 
+
+
+# Stepwise regressions commented out since we give preference to the CV stepwise regs
+
+# LRModel.Step <- stepAIC(LRModel.full, direction = "both", trace = FALSE)
 # Output results to txt file
-sink("LRModel.full.step.txt")
-print(summary(stepAIC(LRModel.full, direction = "both", trace = FALSE)))
-sink()
-# Also have results in R
-LRModel.step <- stepAIC(LRModel.full, direction = "both", trace = FALSE)
-# 
-# # Output results to txt file
-# sink("LRModel.full.forward.txt")
-# print(summary(stepAIC(LRModel.full, direction = "forward", trace = FALSE)))
+# sink("LRModel.full.step.txt")
+# print(summary(LRModel.step))
 # sink()
-# # Also have results in R
+
 # LRModel.forward <- stepAIC(LRModel.full, direction = "forward", trace = FALSE)
-# 
-# # Output results to txt file
-# sink("LRModel.full.backward.txt")
-# print(summary(stepAIC(LRModel.full, direction = "backward", trace = FALSE)))
+# Output results to txt file
+# sink("LRModel.full.forward.txt")
+# print(summary(LRModel.forward))
 # sink()
-# # Also have results in R
+
 # LRModel.backward <- stepAIC(LRModel.full, direction = "backward", trace = FALSE)
-
-
-
+# Output results to txt file
+# sink("LRModel.full.backward.txt")
+# print(summary(LRModel.backward))
+# sink()
 ## Stepwise is currently best preforming model.
-# 10-fold CV with stepwise.
+
+
+
+
+# Begin 10-fold CV with stepwise.
 
 RegData.control <- trainControl(method = "cv", number=10)
 
-LRModel.Step.Train <- train(sum_cost_pedestrian_events ~ ., data=RegData,
+LRModel.Step.Train <- train(sum_cost_pedestrian_events ~ . -cell_id, data=RegData,
                             method = "leapSeq",
                             tuneGrid = data.frame(nvmax = 10:35),
                             trControl = RegData.control)
 
 LRModel.Step.Train$results
 LRModel.Step.Train$bestTune
-#summary(LRModel.Step.Train$finalModel)
-coef(LRModel.Step.Train$finalModel, 30)
 
+
+# DO NOT RUN PAST THIS LINE IF YOU ARE EXPLORING | DO NOT RUN PAST THIS LINE IF YOU ARE EXPLORING | DO NOT RUN PAST THIS LINE IF YOU ARE EXPLORING | 
+#########################################################################################################################################
+
+# Set the number equal to the number output from the last line of code: LRModel.Step.Train$bestTune
+coef(LRModel.Step.Train$finalModel, 30)
+# Manually write all coeficients into the model below
 
 
 # Run linear model of chosen predictors - CV Output
-sink("LRModel.CrossValidatedResult.txt")
-print(summary(lm(sum_cost_pedestrian_events ~ 
-                   access+dblprk+jywalk+lvisib+nobikef+prkint+prkswlk+speed+
-                   vrrlss+wlksig+assist+drives+max_walk_score+sum_area+num_streets+num_fire_incd+med_sale_res_n+
-                   med_sale_com_y+med_sale_com_n+sum_cost.y+dist+n_object+food+police.property+street_sidewalk+trees_plants+
-                   water.leak+zoning_parking+xwalk+n_request 
-                 , data = RegData)))
-sink()
+LRModel.CrossValidatedResult <- lm(formula = sum_cost_pedestrian_events ~ access + dblprk + jywalk + 
+                                            lvisib + nobikef + prkint + prkswlk + speed + vrrlss + wlksig + 
+                                            assist + drives + max_walk_score + sum_area + num_streets + 
+                                            num_fire_incd + med_sale_res_n + med_sale_com_y + med_sale_com_n + 
+                                            sum_cost.y + dist + n_object + food + police.property + street_sidewalk + 
+                                            trees_plants + water.leak + zoning_parking + xwalk + n_request, 
+                                          data = RegData)
 
-LRModel.CrossValidatedResult <- lm(sum_cost_pedestrian_events ~ 
-                                     access+dblprk+jywalk+lvisib+nobikef+prkint+prkswlk+speed+
-                                     vrrlss+wlksig+assist+drives+max_walk_score+sum_area+num_streets+num_fire_incd+med_sale_res_n+
-                                     med_sale_com_y+med_sale_com_n+sum_cost.y+dist+n_object+food+police.property+street_sidewalk+trees_plants+
-                                     water.leak+zoning_parking+xwalk+n_request 
-                                   , data = RegData)
+sink("LRModel.CrossValidatedResult.txt")
+summary(LRModel.CrossValidatedResult)
+sink()
+vif(LRModel.CrossValidatedResult)
+
+
 
 plot(LRModel.CrossValidatedResult$residuals)
 qqnorm(LRModel.CrossValidatedResult$residuals)
@@ -151,29 +162,77 @@ qqline(LRModel.CrossValidatedResult$residuals)
 
 
 
-# Generate predictions
+# Create prediction dataset
 RegDataPred <- RegData
 
 # Create prediction based on model results 
 RegDataPred$predict <- predict(LRModel.CrossValidatedResult, RegData)
 
-# Un-loggify (exponentiate) prediction which was generated in log scale
+
+# Un-loggify (exponentiate) prediction which was generated in log scale - unused
+# Leave commented out if your data is not log transformed, or if it is and you'd like to view the pred vs actual plots in log scale
 #RegDataPred$predict <- exp(RegDataPred$predict)
 
-# Exponentiate the logged columns
+# Exponentiate the logged columns - unused
+# Leave commented out if your data is not log transformed, or if it is and you'd like to view the pred vs actual plots in log scale
 #RegDataPred[Col2Transform] <- exp(RegDataPred[Col2Transform])
 
 
 
-# Create prediction plot
+# Hocus pocus magic line to split the colors
 RegDataPlot <- RegDataPred %>% mutate(cost_gt_pred = ifelse(sum_cost_pedestrian_events > (1.10 * predict), sum_cost_pedestrian_events, NA))
 
 par(bg = "lightgrey")
 plot(sum_cost_pedestrian_events ~ predict, data = RegDataPlot,
-     col = "springgreen4", 
-     xlim = c(0, 5),
-     ylim = c(0, 5),
-     xlab="Predicted Cost ($ in Millions)",
-     ylab="Actual Sum Cost ($ in Millions)")
-points(cost_gt_pred ~ predict, data = RegDataPlot, col = "red4")
-abline(a = 0, b = 1, col = "dodgerblue3")
+     col = "springgreen4", #Type 1 error color
+     xlim = c(0, 5), #adjust to fit plot if in log scale or diff data
+     ylim = c(0, 5), #adjust to fit plot if in log scale or diff data
+     xlab="Predicted Cost ($ in Millions)", #labels
+     ylab="Actual Sum Cost ($ in Millions)") #labels
+points(cost_gt_pred ~ predict, data = RegDataPlot, col = "red4") #Type 2 error color
+abline(a = 0, b = 1, col = "dodgerblue3") #line color
+
+
+#Output residual and cells for visualization later
+PSIFullFinalResiduals <- RegDataPred$sum_cost_pedestrian_events - RegDataPred$predict
+PSIFullFinalcell_id <- RegDataPred$cell_id
+
+setwd(home_dir)
+setwd(data_dir)
+
+PSIFinal <- data.frame(Cell_ID = PSIFullFinalcell_id, Residuals = PSIFullFinalResiduals)
+write.csv(PSIFinal, "Residuals_by_Gridcell_Reduced_Model.csv", row.names = FALSE)
+
+
+
+
+# Final Full Model from CV
+LRModel.CrossValidatedResult <- lm(formula = sum_cost_pedestrian_events ~ access + dblprk + jywalk + 
+                                     lvisib + nobikef + prkint + prkswlk + speed + vrrlss + wlksig + 
+                                     assist + drives + max_walk_score + sum_area + num_streets + 
+                                     num_fire_incd + med_sale_res_n + med_sale_com_y + med_sale_com_n + 
+                                     sum_cost.y + dist + n_object + food + police.property + street_sidewalk + 
+                                     trees_plants + water.leak + zoning_parking + xwalk + n_request, 
+                                   data = RegData)
+
+sink("LRModel.CrossValidatedResult.Full.txt")
+summary(LRModel.CrossValidatedResult)
+sink()
+vif(LRModel.CrossValidatedResult)
+
+
+
+# Final Reduced Model 
+LRModel.CrossValidatedResultReduced <- lm(sum_cost_pedestrian_events ~ 
+                                            lvisib+prkint+prkswlk+speed+
+                                            vrrlss+wlksig+max_walk_score+sum_area+num_streets
+                                          +sum_cost.y+food+police.property+trees_plants+
+                                            water.leak+xwalk
+                                          , data = RegData)
+
+sink("LRModel.CrossValidatedResult.Reduced.txt")
+summary(LRModel.CrossValidatedResultReduced)
+sink()
+vif(LRModel.CrossValidatedResultReduced)
+
+
