403ebd60e890c632160ee1e4f36c3d160083898f
Hymmnos <joshua_herrera@me.com>
2018-12-09 18:02:43 -0500

    Created AUC chart to validate logistic regression.

--- R_scripts/Regressions.R
+++ R_scripts/Regressions.R
@@ -7,6 +7,8 @@ library(schoolmath)
 library(ggplot2)
 library(ggfortify)
 library(e1071)
+library(ROCR)
+
 
 home_dir <- ("G:/JoshuaData/Classes/MSDS61X0 Capstone/CapstoneProject")
 data_dir <- ("./data/")
@@ -15,28 +17,6 @@ plot_dir <- ("./plots/")
 
 setwd(home_dir)
 setwd(data_dir)
-
-# Binary predictor code - unused
-# infile <- "df_model_w_cell_id_ZERO.csv"
-# df_model_w_cell_id_ZERO <- read.csv(infile,
-#                                stringsAsFactors = FALSE, header = TRUE)
-# 
-# 
-# infile <- "random_forest_binary_predictor.csv"
-# BinaryPred <- read.csv(infile,
-#                             stringsAsFactors = FALSE, header = TRUE)
-# 
-# 
-# # Merge predictor to new dataset
-# RegData <- merge(BinaryPred, df_model_w_cell_id_ZERO, by = "cell_id")
-# 
-# # Drop predicted 0-Cost incidents.
-# RegData <- RegData[RegData$binary_predictor == 1,]
-# 
-# 
-# # Add column names to list drop to be dropped from dataset.
-# drop <- c("cell_id", "binary_predictor")
-# RegData <- RegData[, !names(RegData) %in% drop]
 
 
 
@@ -48,51 +28,71 @@ df_model_w_cell_id <- read.csv(infile,
 drop <- c("num_pedestrian_events")
 RegData <- df_model_w_cell_id[, !names(df_model_w_cell_id) %in% drop]
 
-RegData$IsZero <- ifelse(RegData$krnl_cost_pedestrian_events !=0, "Not Zero", "Zero")
+RegData$IsZero <- ifelse(RegData$krnl_cost_pedestrian_events !=0, "0", "1")
 
 set.seed(0737)
 
-
-# Identify columns to log transform - unused
-# 
-#  Col2Transform <- c("krnl_cost_pedestrian_events", 
-#                     "num_near_misses", "access", "dblprk", "dnotyld",
-#                     "jywalk", "lvisib", "lwfws", "nobikef", "noswlk", "other", "prkint", "prkswlk", "speed", "vrrlss", "wlksig", "xwalk",
-#                     "assist", "bikes", "drives", "other.1", "walks", "n_rqst", "mean_walk_score", "min_walk_score", "max_walk_score", 
-#                     "num_walk_scores", "sum_lane_cnt", "sum_width", "sum_area", "num_streets", "num_fire_incd", "med_sale_res_y", 
-#                     "med_sale_res_n", "med_sale_com_y", "med_sale_com_n", "med_sale_ind_y", "med_sale_ind_n", "med_sale_pbo_y", 
-#                     "med_sale_pbo_n", "sum_cost_non_pedestrian_events", "num_non_pedestrian_events", "dist", "n_object",
-#                     "animals_insects", "building.related", "construction", "food", "others", "police.property", "service.complaint", 
-#                     "street_sidewalk", "traffic_signal", "trash", "trees_plants", "water.leak", "zoning_parking", "n_request")
-#  
-#  RegData[RegData == 0] <- .001
-#  
-#  RegData[Col2Transform] <- log(RegData[Col2Transform])
-#  
+  
 
 #####
 ##### Begin Regressions
 #####
 
-# Logistic Regression
 
 
 
-# Begin 10-fold CV with stepwise.
 
+# Setup 10-fold CV
 RegData.control <- trainControl(method = "cv", number=10)
 
+# Logistic Regression
 LogRModel <- train(IsZero ~ . -krnl_cost_pedestrian_events -cell_id, data=RegData,
                    method = "glm", family="binomial", trControl = RegData.control)
 
 LogRModel$results
 
 
+
 # Create prediction based on model results 
 RegData$predictlogistic <- predict(LogRModel, RegData)
 
+#########################################################################################################
+
+# AUC of predicted class probabilities
+
+prob <- predict(LogRModel, newdata=RegData, type="prob")
+pred <- prediction(prob$`1`, RegData$IsZero)
+perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 
 
+df_roc <- data.frame(perf@x.values, perf@y.values)
+colnames(df_roc) <- c("FPR", "TPR")
+
+
+roc_plot <- ggplot(df_roc, aes(x = FPR, y = TPR)) +
+  geom_point(color = "#666666") +
+  geom_line(color = "#5577FF", size = 2) +
+  #    geom_smooth() +
+  ggtitle("AUC of Predicted Class Probabilities") +
+  xlab("False Positive Rate") + ylab("True Positive Rate") +
+  theme(text = element_text(size = 15))
+
+roc_plot
+
+setwd(home_dir)
+setwd(plot_dir)
+
+ggsave(filename = "Logistic_Model_HD.png", width = 24, height = 13.4, units = "in", dpi = 100.13)
+ggsave(filename = "Logistic_Model.png", width = 16, height = 9, units = "in")
+
+auc <- performance(pred, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
+###################################################################################################
+
+
+# 10-fold CV Stepwise Regression
 LRModel.Step.Train <- train(krnl_cost_pedestrian_events ~ . -cell_id -IsZero -predictlogistic, data=RegData,
                             method = "leapSeq",
                             tuneGrid = data.frame(nvmax = 1:40),
@@ -124,13 +124,6 @@ sink()
 vif(LRModel.CrossValidatedResult)
 
 
-
-plot(LRModel.CrossValidatedResult$residuals)
-qqnorm(LRModel.CrossValidatedResult$residuals)
-qqline(LRModel.CrossValidatedResult$residuals)
-
-
-
 # Create prediction dataset
 RegDataPred <- RegData
 
@@ -138,16 +131,6 @@ RegDataPred <- RegData
 RegDataPred$predict <- predict(LRModel.CrossValidatedResult, RegData)
 # Re-add zeros from logistic model
 RegDataPred$predict[RegDataPred$predictlogistic == "Zero"] <- 0
-
-
-# Un-loggify (exponentiate) prediction which was generated in log scale - unused
-# Leave commented out if your data is not log transformed, or if it is and you'd like to view the pred vs actual plots in log scale
-#RegDataPred$predict <- exp(RegDataPred$predict)
-
-# Exponentiate the logged columns - unused
-# Leave commented out if your data is not log transformed, or if it is and you'd like to view the pred vs actual plots in log scale
-#RegDataPred[Col2Transform] <- exp(RegDataPred[Col2Transform])
-
 
 
 # Residuals used to split color on chart
@@ -319,4 +302,12 @@ setwd(data_dir)
 ResidualCSV <- data.frame(Cell_ID = PSIFullFinalcell_id, Residuals = PSIFullFinalResiduals)
 write.csv(ResidualCSV, "Residuals_by_Gridcell_Reduced_Model.csv", row.names = FALSE)
 
+
+
+#####################
+# Model Comparison and Evaluation
+####################
+
+# Stepwise vs Manual Pruning. Manual Pruning Statistically Different
+anova(LRModel.CrossValidatedResultFull, LRModel.CrossValidatedResultReduced, test = "F")
 
